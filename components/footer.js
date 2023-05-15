d4772e15ed187ae904ccffdde2a0cdba2f59538a
kumarsuraj151 <102572879+kumarsuraj151@users.noreply.github.com>
2023-05-15 11:05:17 +0530

    removing old footer

--- components/footer.js
+++ components/footer.js
@@ -1,128 +1,128 @@
-import { ReactNode } from 'react';
-import {
-  Box,
-  Container,
-  Stack,
-  SimpleGrid,
-  Text,
-  Link,
-  VisuallyHidden,
-  chakra,
-  useColorModeValue,
-  Heading,
-} from '@chakra-ui/react';
-import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
+// import { ReactNode } from 'react';
+// import {
+//   Box,
+//   Container,
+//   Stack,
+//   SimpleGrid,
+//   Text,
+//   Link,
+//   VisuallyHidden,
+//   chakra,
+//   useColorModeValue,
+//   Heading,
+// } from '@chakra-ui/react';
+// import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
 
-// import AppStoreBadge from '@/components/AppStoreBadge';
-// import PlayStoreBadge from '@/components/PlayStoreBadge';
+// // import AppStoreBadge from '@/components/AppStoreBadge';
+// // import PlayStoreBadge from '@/components/PlayStoreBadge';
 
-const ListHeader = () => {
-  return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
-      {/* {children} */}
-    </Text>
-  );
-};
+// const ListHeader = () => {
+//   return (
+//     <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+//       {/* {children} */}
+//     </Text>
+//   );
+// };
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
+// const SocialButton = ({
+//   children,
+//   label,
+//   href,
+// }) => {
+//   return (
+//     <chakra.button
+//       bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+//       rounded={'full'}
+//       w={8}
+//       h={8}
+//       cursor={'pointer'}
+//       as={'a'}
+//       href={href}
+//       display={'inline-flex'}
+//       alignItems={'center'}
+//       justifyContent={'center'}
+//       transition={'background 0.3s ease'}
+//       _hover={{
+//         bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+//       }}>
+//       <VisuallyHidden>{label}</VisuallyHidden>
+//       {children}
+//     </chakra.button>
+//   );
+// };
 
-export default function LargeWithAppLinksAndSocial() {
-  return (
-    <footer>
-    <Box
-      bg={'#0f1619'}
-      color={'white'}>
-      <Container as={Stack} maxW={'6xl'} py={10}>
-        <div className='flex felx-row justify-evenly mb-5'>
-            <Heading fontSize={'2rem'}>Pool & Save</Heading>
-            <Stack direction={'row'} spacing={6} alignItems={"center"}>
-              <SocialButton label={'Twitter'} href={'#'}>
-                <FaTwitter  size={30}/>
-              </SocialButton>
-              <SocialButton label={'YouTube'} href={'#'}>
-                <FaYoutube size={30} />
-              </SocialButton>
-              <SocialButton label={'Instagram'} href={'#'}>
-                <FaInstagram size={30} />
-              </SocialButton>
-            </Stack>
-        </div>
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}  justifyContent={"space-evenly"}>
-          <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
-            <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Company</Heading>
-            <Link href={'#'}>About Us</Link>
-            <Link href={'#'}>Blog</Link>
-            <Link href={'#'}>Careers</Link>
-            <Link href={'#'}>Contact Us</Link>
-          </Stack>
+// export default function LargeWithAppLinksAndSocial() {
+//   return (
+//     <footer>
+//     <Box
+//       bg={'#0f1619'}
+//       color={'white'}>
+//       <Container as={Stack} maxW={'6xl'} py={10}>
+//         <div className='flex felx-row justify-evenly mb-5'>
+//             <Heading fontSize={'2rem'}>Pool & Save</Heading>
+//             <Stack direction={'row'} spacing={6} alignItems={"center"}>
+//               <SocialButton label={'Twitter'} href={'#'}>
+//                 <FaTwitter  size={30}/>
+//               </SocialButton>
+//               <SocialButton label={'YouTube'} href={'#'}>
+//                 <FaYoutube size={30} />
+//               </SocialButton>
+//               <SocialButton label={'Instagram'} href={'#'}>
+//                 <FaInstagram size={30} />
+//               </SocialButton>
+//             </Stack>
+//         </div>
+//         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}  justifyContent={"space-evenly"}>
+//           <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
+//             <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Company</Heading>
+//             <Link href={'#'}>About Us</Link>
+//             <Link href={'#'}>Blog</Link>
+//             <Link href={'#'}>Careers</Link>
+//             <Link href={'#'}>Contact Us</Link>
+//           </Stack>
 
-            <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
-            <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Support</Heading>
-            <Link href={'#'}>Help center</Link>
-            <Link href={'#'}>Safety Center</Link>
-            <Link href={'#'}>Community Guidelines</Link>
-          </Stack>
+//             <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
+//             <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Support</Heading>
+//             <Link href={'#'}>Help center</Link>
+//             <Link href={'#'}>Safety Center</Link>
+//             <Link href={'#'}>Community Guidelines</Link>
+//           </Stack>
 
-            <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
-            <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Legal</Heading>
-            <Link href={'#'}>Cookies Policy</Link>
-            <Link href={'#'}>Privacy Policy</Link>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Law Enforcement</Link>
-          </Stack>
+//             <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
+//             <Heading as={'h4'} color='grey' fontSize={'1.5rem'}>Legal</Heading>
+//             <Link href={'#'}>Cookies Policy</Link>
+//             <Link href={'#'}>Privacy Policy</Link>
+//             <Link href={'#'}>Terms of Service</Link>
+//             <Link href={'#'}>Law Enforcement</Link>
+//           </Stack>
 
-            <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
-            <ListHeader>Install App</ListHeader>
-            {/* <AppStoreBadge />
-            <PlayStoreBadge /> */}
-            <div>AppStoreBadge</div>
-            <div>PlayStoreBadge</div>
-          </Stack>
-        </SimpleGrid>
-      </Container>
+//             <Stack align={'flex-start'} width={"fit-content"} marginX={"auto"}>
+//             <ListHeader>Install App</ListHeader>
+//             {/* <AppStoreBadge />
+//             <PlayStoreBadge /> */}
+//             <div>AppStoreBadge</div>
+//             <div>PlayStoreBadge</div>
+//           </Stack>
+//         </SimpleGrid>
+//       </Container>
 
-      <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={4}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ md: 'space-between' }}
-          align={{ md: 'center' }}>
-          <Text display={'block'} margin='auto'>© Copyrights Pool & Save  All rights reserved</Text>
+//       <Box
+//         borderTopWidth={1}
+//         borderStyle={'solid'}
+//         borderColor={useColorModeValue('gray.200', 'gray.700')}>
+//         <Container
+//           as={Stack}
+//           maxW={'6xl'}
+//           py={4}
+//           direction={{ base: 'column', md: 'row' }}
+//           spacing={4}
+//           justify={{ md: 'space-between' }}
+//           align={{ md: 'center' }}>
+//           <Text display={'block'} margin='auto'>© Copyrights Pool & Save  All rights reserved</Text>
           
-        </Container>
-      </Box>
-    </Box>
-    </footer>
-  );
-}
+//         </Container>
+//       </Box>
+//     </Box>
+//     </footer>
+//   );
+// }
