7212be818db43cd3a4f11dd78a3cd083c979582b
jaswanth <jaswanthbeere@gmail.com>
2023-05-26 01:20:08 +0530

    updated chat interface

--- components/MyChats.js
+++ components/MyChats.js
@@ -1,17 +1,17 @@
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Stack, Text } from "@chakra-ui/layout";
-import { useToast } from "@chakra-ui/toast";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { getSender } from "../config/ChatLogics";
-import ChatLoading from "./ChatLoading";
-import GroupChatModal from "./miscellaneous/GroupChatModal";
-import { Button } from "@chakra-ui/react";
-import { ChatState } from "../Context/ChatProvider";
-import secureLocalStorage from "react-secure-storage";
-import Router from "next/router";
-import { ClassNames } from "@emotion/react";
-import { getCookie, deleteCookie } from "cookies-next";
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, Stack, Text } from '@chakra-ui/layout';
+import { useToast } from '@chakra-ui/toast';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { getSender } from '../config/ChatLogics';
+import ChatLoading from './ChatLoading';
+import GroupChatModal from './miscellaneous/GroupChatModal';
+import { Avatar, Button } from '@chakra-ui/react';
+import { ChatState } from '../Context/ChatProvider';
+import secureLocalStorage from 'react-secure-storage';
+import Router from 'next/router';
+import { ClassNames } from '@emotion/react';
+import { getCookie, deleteCookie } from 'cookies-next';
 
 export default function MyChats({ fetchAgain }) {
   const [loggedUser, setLoggedUser] = useState([]);
@@ -24,29 +24,29 @@ export default function MyChats({ fetchAgain }) {
   const fetchChats = async () => {
     // console.log(user._id);
     try {
-      const res = await fetch("/api/chat/fetchChat", {
-        method: "GET", // or 'PUT'
+      const res = await fetch('/api/chat/fetchChat', {
+        method: 'GET', // or 'PUT'
       });
       let data = await res.json();
       // console.log(data)
       setChats(data);
     } catch (error) {
       toast({
-        title: "Error Occured!",
-        description: "Failed to Load the chats",
-        status: "error",
+        title: 'Error Occured!',
+        description: 'Failed to Load the chats',
+        status: 'error',
         duration: 5000,
         isClosable: true,
-        position: "bottom-left",
+        position: 'bottom-left',
       });
     }
   };
 
   useEffect(() => {
-    setLoggedUser(JSON.parse(localStorage.getItem("user")));
+    setLoggedUser(JSON.parse(localStorage.getItem('user')));
     if (
-      getCookie("authtoken") !== null &&
-      getCookie("authtoken") !== undefined
+      getCookie('authtoken') !== null &&
+      getCookie('authtoken') !== undefined
     ) {
       fetchChats();
     }
@@ -54,79 +54,110 @@ export default function MyChats({ fetchAgain }) {
   return (
     <Box
       // d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-      className={selectedChat ? "selecof" : "selecon"}
+      className={selectedChat ? 'selecof' : 'selecon'}
       flexDir="column"
       alignItems="center"
       p={3}
       bg="white"
-      w={{ base: "100%", md: "31%" }}
+      w={{ base: '100%', md: '31%' }}
       borderRadius="lg"
-      borderWidth="1px"
+      // borderWidth="1px"
     >
       <Box
         pb={3}
         px={3}
-        fontSize={{ base: "28px", md: "30px" }}
-        fontFamily="Work sans"
+        fontSize={{ base: '28px', md: '30px' }}
         d="flex"
         w="100%"
         justifyContent="space-between"
         alignItems="center"
+        sx={{
+          color: 'rgba(0, 0, 0, 0.7)',
+        }}
       >
         My Chats
         <GroupChatModal>
           <Button
             d="flex"
-            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
-            rightIcon={<AddIcon />}
+            fontSize={{ base: '17px', md: '10px', lg: '17px' }}
+            sx={{
+              fontWeight: '400',
+            }}
+            rightIcon={<AddIcon sx={{ fontSize: '.8rem' }} />}
+            ml="10px"
           >
-            New Group Chat
+            Create Group
           </Button>
         </GroupChatModal>
       </Box>
-      <Box
-        d="flex"
-        flexDir="column"
-        p={3}
-        bg="#F8F8F8"
-        w="100%"
-        borderRadius="lg"
-        overflowY="scroll"
-      >
+      <div className="bg-[#b9e9fc]/20">
         {chats ? (
-          <Stack>
+          <div className="flex flex-col w-full py-5 overflow-y-scrol ">
             {chats &&
               chats.map((chat) => (
-                <Box
+                <div
                   onClick={() => setSelectedChat(chat)}
-                  cursor="pointer"
-                  bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                  color={selectedChat === chat ? "white" : "black"}
-                  px={3}
-                  py={2}
-                  borderRadius="lg"
                   key={chat._id}
+                  className="border-b border-black/10 cursor-pointer py-2 px-2 hover:bg-[#b9e9fc]/40"
                 >
-                  <Text>
-                    {!chat.isGroupChat
-                      ? getSender(loggedUser, chat.users)
-                      : chat.chatName}
-                  </Text>
-                  {chat.latestMessage && (
-                    <Text fontSize="xs">
-                      <b>{chat.latestMessage.sender.name} : </b>
-                      {chat.latestMessage.content.length > 50
-                        ? chat.latestMessage.content.substring(0, 51) + "..."
-                        : chat.latestMessage.content}
-                    </Text>
-                  )}
-                </Box>
+                  <div className="flex items-center justify-centr">
+                    <Avatar
+                      mr={2}
+                      size="sm"
+                      cursor="pointer"
+                      name={user.name}
+                      src={user.pic}
+                    />
+
+                    <div className="flex flex-col w-full">
+                      <div className="flex flex-row justify-between w-full">
+                        <p>
+                          {!chat.isGroupChat
+                            ? getSender(loggedUser, chat.users)
+                            : chat.chatName}
+                        </p>
+                        <p className="text-gray-500 text-[.8rem]">18:14</p>
+                      </div>
+
+                      <div>
+                        {chat.latestMessage && (
+                          <div className="flex justify-start items-center pb-2 gap-1">
+                            <p className="text-gray-500 text-[.8rem]">
+                              {chat.latestMessage.sender.name}:{' '}
+                            </p>
+                            <p className="text-gray-500 text-[.8rem]">
+                              {chat.latestMessage.content.length > 50
+                                ? chat.latestMessage.content.substring(0, 51) +
+                                  '...'
+                                : chat.latestMessage.content}
+                            </p>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               ))}
-          </Stack>
+          </div>
         ) : (
           <ChatLoading />
         )}
-      </Box>
+      </div>
     </Box>
   );
 }
+
+{
+  /* <b>{chat.latestMessage.sender.name} : </b> */
+}
+
+// cursor="pointer"
+// bg={selectedChat === chat ? '#38B2AC' : 'white'}
+// color={selectedChat === chat ? 'white' : 'black'}
+// sx={{
+//   backgroundColor: 'white',
+//   padding: '10px',
+//   // margin: '10px 0',
+//   borderRadius: '5px',
+//   boxShadow: 'rgba(0, 0, 0, 0.18) 0px 2px 4px',
+// }}
--- pages/chat.js
+++ pages/chat.js
@@ -1,44 +1,52 @@
-import { useState, useEffect } from "react";
-import { ChatState } from "../Context/ChatProvider";
-import Router from "next/router";
-import { Box } from "@chakra-ui/react";
-import SideDrawer from "../components/SideDrawer";
-import MyChats from "../components/MyChats";
-import Chatbox from "../components/Chatbox";
-import secureLocalStorage from "react-secure-storage";
-import { getCookie, deleteCookie } from "cookies-next";
-import { NextSeo } from "next-seo";
+import { useState, useEffect } from 'react';
+import { ChatState } from '../Context/ChatProvider';
+import Router from 'next/router';
+import { Box } from '@chakra-ui/react';
+import SideDrawer from '../components/SideDrawer';
+import MyChats from '../components/MyChats';
+import Chatbox from '../components/Chatbox';
+import secureLocalStorage from 'react-secure-storage';
+import { getCookie, deleteCookie } from 'cookies-next';
+import { NextSeo } from 'next-seo';
+import NavbarT2 from '../components/NavbarT2';
+import Footer from '../components/FooterT2';
 
 export default function Chat() {
   const [fetchAgain, setFetchAgain] = useState(false);
   const { user } = ChatState();
   useEffect(() => {
-    if (getCookie("authtoken") == null && getCookie("authtoken") == undefined) {
-      Router.push("/");
+    if (getCookie('authtoken') == null && getCookie('authtoken') == undefined) {
+      Router.push('/');
     }
   }, []);
 
   return (
-    <>
-    <NextSeo
-        title="Chat"
-        description="This is user Chat Page"
-      />
-    <div style={{ width: "100%" }}>
-      {user && <SideDrawer />}
-      <Box
-        className="flex"
-        justifyContent="space-between"
-        w="100%"
-        h="91.5vh"
-        p="10px"
-      >
-        {user && <MyChats fetchAgain={fetchAgain} />}
-        {user && (
-          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-        )}
-      </Box>
+    <div className="bg-[#B9E9FC]">
+      <NextSeo title="Chat" description="This is user Chat Page" />
+      <div className="flex flex-col  items-center justify-center mx-auto">
+        <div className="w-[90%]">
+          <NavbarT2 />
+          <div className="py-[2rem] w-full">
+            {<SideDrawer />}
+            <Box
+              className="flex"
+              justifyContent="space-between"
+              w="100%"
+              h="73.5vh"
+              p="10px 0"
+            >
+              {<MyChats fetchAgain={fetchAgain} />}
+              {
+                <Chatbox
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
+              }
+            </Box>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
-    </>
   );
 }
