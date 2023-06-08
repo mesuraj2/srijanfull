23c7cd8e6be1735c8a0de6524075f0eede0d1127
jaswanth <jaswanthbeere@gmail.com>
2023-06-08 22:32:42 +0530

    removed cart in offers page

--- pages/offer/[offerDetail].js
+++ pages/offer/[offerDetail].js
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import Carousel from "../../components/Carousel";
-import FooterT2 from "../../components/FooterT2";
-import NavbarT2 from "../../components/NavbarT2";
-import OfferBadges from "../../components/OfferBadges";
-import SearchBar from "../../components/SearchBar";
-import { ImLocation } from "react-icons/im";
-import axios from "axios";
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import Carousel from '../../components/Carousel';
+import FooterT2 from '../../components/FooterT2';
+import NavbarT2 from '../../components/NavbarT2';
+import OfferBadges from '../../components/OfferBadges';
+import SearchBar from '../../components/SearchBar';
+import { ImLocation } from 'react-icons/im';
+import axios from 'axios';
 
 const Index = ({ data }) => {
   let imglinks = new Object();
@@ -22,15 +22,15 @@ const Index = ({ data }) => {
     <div className="w-screen bg-[#B9E9FC]">
       <div className="flex items-center justify-center mx-auto mt-10 lg:my-10">
         <SearchBar
-          globalClassName={""}
-          inputClassName={"w-[100%] md:w-[35rem]"}
+          globalClassName={''}
+          inputClassName={'w-[100%] md:w-[35rem]'}
         />
       </div>
       <div className="pb-[2rem] pt-[4rem]">
         <div className="flex flex-col md:flex-row gap-5 lg:gap-10 ">
-          <div className="14xl:w-[28rem] 14xl:h-[28rem] w-[18rem] h-[18rem] xs:w-[20rem] xs:h-[20rem] md:w-[24rem] md:h-[24rem] rounded-md flex flex-col items-center justify-center mx-auto md:mx-0">
+          <div className="14xl:w-[28rem] 14xl:h-[28rem] w-[16rem] h-[16rem] xs:w-[20rem] xs:h-[20rem] md:w-[24rem] md:h-[24rem] rounded-md flex flex-col items-center justify-center mx-auto md:mx-0">
             <Carousel imageLinks={imglinks} />
-            <button className="btn w-fit btn-outline flex flex-row gap-2 items-center mt-5 mx-auto">
+            {/* <button className="btn w-fit btn-outline flex flex-row gap-2 items-center mt-5 mx-auto">
               <p>Add to Cart</p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -46,17 +46,17 @@ const Index = ({ data }) => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-            </button>
+            </button> */}
           </div>
-          <div className=" flex flex-col gap-5 items-center justify-center md:items-start md:justify-start mt-[5rem] md:mt-10">
+          <div className=" flex flex-col gap-5 items-center justify-center md:items-start md:justify-start mt-[5rem] md:mt-10 px-2 md:px-0">
             <div className="flex flex-row gap-10 items-center">
               <p className="uppercase text-black/70 text-[1.5rem] main__font  font-[600]">
                 {data.offername}
               </p>
-              {data.tags ? <OfferBadges badgeList={data.tags} /> : ""}
+              {data.tags ? <OfferBadges badgeList={data.tags} /> : ''}
             </div>
             <h2 className="tracking-wider text-black/90 secondary_font text-[1.3rem]">
-              {data.smalldescription ? data.smalldescription : ""}
+              {data.smalldescription ? data.smalldescription : ''}
             </h2>
             <p className="tracking-wider text-black/70 secondary_font text-[1.15rem]">
               {data.description}
@@ -68,7 +68,7 @@ const Index = ({ data }) => {
                   <p>{data.locationdescription}</p>
                 </div>
               ) : (
-                ""
+                ''
               )}
             </div>
 
@@ -76,7 +76,7 @@ const Index = ({ data }) => {
               onClick={() =>
                 router.push(
                   `/radar/${data._id}?location=${localStorage.getItem(
-                    "coordinates"
+                    'coordinates'
                   )}`
                 )
               }
@@ -95,9 +95,12 @@ export default Index;
 
 export async function getServerSideProps(context) {
   const offerid = context.query.offerDetail;
-  const { data } = await axios.post(`${process.env.DOMAIN_URI}/api/offer/offerchats`, {
-    id: offerid,
-  });
+  const { data } = await axios.post(
+    `${process.env.DOMAIN_URI}/api/offer/offerchats`,
+    {
+      id: offerid,
+    }
+  );
   console.log(data);
   return {
     props: { data: data }, // will be passed to the page component as props
