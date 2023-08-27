04f212be81e51d4210fe19cd549704b3c0d3c4dc
kumarsuraj151 <102572879+kumarsuraj151@users.noreply.github.com>
2023-08-27 12:17:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,74 +57,22 @@ app
       console.log(`Server running on  http://localhost:3000`)
     );
 
-    // const io = require("socket.io")(server2, {
-    //   pingTimeout: 60000,
-    //   cors: {
-    //     // origin: ["www.picapool.com", "picapool.com"],
-    //     // credentials: true,
-    //   },
-    // });
+    const changeStream = notification.watch();
 
-    // io.on("connection", (socket) => {
-    //   // //console.log("Connected to via locally");
-    //   socket.on("setup", (userData) => {
-    //     // //console.log(userData);
-    //     socket.join(userData);
-    //     socket.emit("connected");
-    //   });
+    io.on("connection", (socket) => {
+      // //console.log("Connected to via locally");
 
-    //   socket.on("join chat", (room) => {
-    //     socket.join(room);
-    //     // //console.log("User Joined Room: " + room);
-    //   });
-    //   socket.on("typing", (room) => socket.in(room).emit("typing"));
-    //   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+      changeStream.on('change', (change) => {
+        if (change.operationType === 'insert') {
+          socket.emit('data', change.fullDocument);
+        }
+      });
 
-    //   socket.on("new offerchat", (newMessageRecieved, userId) => {
-    //     newMessageRecieved.forEach((user) => {
-    //       if (user.user) {
-    //         if (user.user._id == userId) return;
-    //         socket
-    //           .in(user.user._id)
-    //           .emit("newChatNotification", user.user.latestNotif);
-    //       }
-    //     });
-    //   });
-
-      // socket.on("new message", (newMessageRecieved) => {
-      //   // //console.log(newMessageRecieved)
-      //   var chat = newMessageRecieved.chat;
-      //   if (!chat.users) return //console.log("chat.users not defined");
-      //   chat.users.forEach((user) => {
-      //     // //console.log(user._id);
-      //     if (user._id == newMessageRecieved.sender._id) return;
-      //     // //console.log("suraj")
-      //     socket.in(user._id).emit("message recieved", newMessageRecieved);
-      //     // socket.emit("message recieved", newMessageRecieved);
-      //   });
-      // });
-
-    //   socket.on('new_message', (message) => {
-    //     console.log(`Message:${socket.id} ${message.content}`);
-    //     socket.to(message.room).emit('recieve_message', message)
-    //   })
-
-    //   socket.on("newchat", (newMessageRecieved, userId) => {
-    //     console.log('new message')
-    //     newMessageRecieved.forEach((user) => {
-    //       if (user.user) {
-    //         if (user.user._id == userId) return;
-    //         console.log(user.user)
-    //         socket.in(user.user._id).emit("new_notification", user.user.latestNotif);
-    //       }
-    //     });
-    //   });
-
-    //   socket.off("setup", () => {
-    //     // //console.log("USER DISCONNECTED");
-    //     socket.leave(userData._id);
-    //   });
-    // });
+      socket.on('disconnect', () => {
+        console.log('Client disconnected');
+      });
+    });
+  })
   })
   .catch((ex) => {
     console.error(ex.stack);
