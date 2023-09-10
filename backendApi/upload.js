ce72e3ebabc9b38877fd7d01c5c62acc1fbf131b
adhitht <adhith.tharammal@gmail.com>
2023-09-11 01:47:19 +0530

    Update with digitalocean space

--- backendApi/upload.js
+++ backendApi/upload.js
@@ -16,7 +16,7 @@ const s3 = new aws.S3({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "picapoolcdn",
+    bucket: "picapoolbeta",
     acl: "public-read",
     key: function (req, file, cb) {
       // //console.log(file);
